711dac599d00c012107e15e29f1c30547c4d40c8
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2021-10-16 15:32:39 +0300

    Validating the email input in the forgotpassword session

---
+++ javaScriptfiles/forgotpassword.js
@@ -0,0 +1,29 @@
+/* eslint-disable eqeqeq */
+const emailFormat = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
+
+const Email = document.forgotpassword.email;
+
+const EmailError = document.getElementById('email-error');
+
+const validate = () => {
+  if (Email.value == '') {
+    EmailError.style = 'color: red';
+    EmailError.innerHTML = 'Enter Email Address';
+    Email.style.border = '1px solid red';
+    Email.focus();
+  } else if (!Email.value.match(emailFormat)) {
+    EmailError.innerHTML = 'Email Format entered is Wrong';
+    EmailError.style = 'color:red';
+    Email.style.border = '1px solid red';
+    Email.focus();
+  } else {
+    EmailError.innerHTML = 'Field correctly filled, Thank You ';
+    EmailError.style = 'color:#fff ';
+  }
+};
+
+const ForgotPasswordform = document.getElementById('form-id');
+ForgotPasswordform.addEventListener('submit', validate);
+ForgotPasswordform.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
