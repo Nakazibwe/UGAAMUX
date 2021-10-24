0c84e0c85b9978d39ea7eea7d6350c23ce03a0c3
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2021-10-25 01:00:27 +0300

    Schemers edits

--- models/bandRegModel.js
+++ models/bandRegModel.js
@@ -10,83 +10,100 @@ const BandRegSchema = new mongoose.Schema({
     BandName: {
         type: String,
         trim: true,
+        default: '',
     },
     BandOwner: {
         type: String,
         trim: true,
+        default: '',
     },
     BandHome: {
         type: String,
-        trim: true,            
+        trim: true,
+        default: '',
         
     },
     BandNumber: {
         type: Number,
         trim: true,
+        default: 0,
     },
     BandSlogan: {
         type: String,
         trim: true,
+        default: '',
     },
     bandID: {
         type: String,
         trim: true,
-                
-        
+        default: '',
+                   
     },
     numberofsongs: {
         type: Number,
         trim: true,
+        default: 0,
     },
     DateFormation: {
-        type: String,
+        type: Date,
         trim: true,
+        
     },
     telephone: {
         type: String,
         trim: true,
-                
-        
+        default: '',
+                  
     },
     twitter: {
         type: String,
         trim: true,
+        default: '',
     },
     email: {
         type: String,
         trim: true,
+        unique: true,
+        default: '',
     },
     bandCrowns: {
         type: String,
         trim: true,
+        default: '',
                 
-    
     },
     albums: {
         type: Number,
         trim: true,
+        default: '',
     },
     genre: {
-        // type: Image,
+        type: String,
+        trim: true,
+        default: '',
           
     },
     icon: {
-        // type: Image,
+        data: Buffer,
+        contentType: String,
         
     },
     Profilepicture: {
-        // type: Image,
+        data: Buffer,
+        contentType: String,
                    
     },
     ownership: {
         type: String,
+        default: '',
         
     },
     bandsponsors: {
         type: String,
-        trim: true,
+        default: '',
+        
     },
     
    
-});      
+});       
 module.exports = mongoose.model('BandReg', BandRegSchema);
