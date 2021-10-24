bf84a104525e1047a13f9ee17705fd27f4664bc8
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2021-10-25 01:01:14 +0300

    Schemer edits

--- models/comedianRegModel.js
+++ models/comedianRegModel.js
@@ -10,71 +10,76 @@ const ComedianRegSchema = new mongoose.Schema({
     firstName: {
         type: String,
         trim: true,
+        default: '',
     },
     lastName: {
         type: String,
         trim: true,
+        default: '',
     },
     stagename: {
         type: String,
         trim: true,
-           
-        
+        default: '',
+               
     },
     comedianID: {
         type: String,
         trim: true,
-    },
-    customeremail: {
-        type: String,
-        trim: true,
+        default: '',
     },
     comedianphone: {
         type: String,
-           
-        
+        trim: true,
+        default: '',
+              
     },
     NationalNIN: {
         type: String,
         trim: true,
+        default: '',
     },
     twitter: {
         type: String,
         trim: true,
+        default: '',
     },
     email: {
         type: String,
         trim: true,
         unique: true,
-           
-        
+        default: '',
+               
     },
     ComedianDOB: {
-        type: String,
+        type: Date,
         trim: true,
     },
     Location: {
         type: String,
         trim: true,
+        default: '',
     },
     Based: {
         type: String,
-           
-        
+        trim: true,
+        default: '',
+             
     },
     startedcomedy: {
-        type: String,
+        type: Date,
         trim: true,
     },
     gender: {
         type: String,
         trim: true,
+        default: '',
     },
     uploadedpicture: {
-        // type: Image,
-           
-        
-    },
+        data: Buffer,
+        contentType: String,
+              
+    },  
     
 });      
 module.exports = mongoose.model('ComedianReg', ComedianRegSchema);
