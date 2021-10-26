b048e59ac7fad3eda9eee4a95bace55bdcaa62d1
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2021-10-26 20:40:33 +0300

    SAdditions were made to schemas

--- models/forgotpasswdModel.js
+++ models/forgotpasswdModel.js
@@ -1,4 +1,4 @@
-   /* eslint-disable no-multiple-empty-lines */
+/* eslint-disable no-multiple-empty-lines */
 /* eslint-disable indent */
    /* eslint-disable no-trailing-spaces */
 /* eslint-disable no-unused-vars */
@@ -7,11 +7,10 @@ const mongoose = require('mongoose');
 
 const ForgotpasswdSchema = new mongoose.Schema({
     
-  
     email: {
         type: String,
         trim: true,
-        default: '',
+        required: true, 
     },
 });      
 module.exports = mongoose.model('ForgotPassword', ForgotpasswdSchema);
