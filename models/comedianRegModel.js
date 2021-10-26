96919bfd57798523e7111e5c9e5539b3ee0b8df1
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2021-10-26 20:35:33 +0300

    Additions were made to the schema

--- models/comedianRegModel.js
+++ models/comedianRegModel.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-multiple-empty-lines */
 /* eslint-disable indent */
-   /* eslint-disable no-trailing-spaces */
+/* eslint-disable no-trailing-spaces */
 /* eslint-disable no-unused-vars */
 /* eslint-disable indent */
 const mongoose = require('mongoose');
@@ -10,72 +10,73 @@ const ComedianRegSchema = new mongoose.Schema({
     firstName: {
         type: String,
         trim: true,
-        default: '',
+        required: true,
     },
     lastName: {
         type: String,
         trim: true,
-        default: '',
+        required: true,
     },
     stagename: {
         type: String,
         trim: true,
-        default: '',
-               
+        required: true,        
     },
     comedianID: {
         type: String,
         trim: true,
-        default: '',
+        required: true,
     },
     comedianphone: {
         type: String,
         trim: true,
-        default: '',
-              
+        required: true,         
     },
     NationalNIN: {
         type: String,
         trim: true,
-        default: '',
+        required: true,
     },
     twitter: {
         type: String,
         trim: true,
-        default: '',
+        required: true,
     },
     email: {
         type: String,
         trim: true,
         unique: true,
-        default: '',
+        required: true,
                
     },
     ComedianDOB: {
         type: Date,
         trim: true,
+        required: true,
     },
-    Location: {
+    Location: {   
         type: String,
         trim: true,
-        default: '',
+        required: true,
     },
     Based: {
         type: String,
         trim: true,
-        default: '',
+        required: true,
              
     },
     startedcomedy: {
         type: Date,
         trim: true,
+        required: true,
     },
     gender: {
         type: String,
         trim: true,
-        default: '',
+        required: true,
     },
     uploadedpicture: {
+        
         data: Buffer,
         contentType: String,
               
