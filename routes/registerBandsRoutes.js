a11e4881b957bbb5928b0f56381ba88d01c609c6
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2021-10-27 19:36:00 +0300

    Changes were made to the routes of band registration

--- routes/registerBandsRoutes.js
+++ routes/registerBandsRoutes.js
@@ -1,7 +1,21 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
 /* eslint-disable no-path-concat */
 /* eslint-disable prefer-template */
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const BandReg = require('./models/bandRegModel');
+const express = require('express');
+const mongoose = require('mongoose');
+const BandReg = require('../models/bandRegModel');
 
-// const router = express.Router();
+const router = express.Router();
+
+// Route to the bands registration page.
+router.get('/bandregistrationform', (req, res) => {
+  res.render('bandregform');
+});
+
+// Route to post from  band registration page.
+router.post('/bandregistrationform', (req, res) => {
+  console.log(req.body);
+});
+
+module.exports = router; 
