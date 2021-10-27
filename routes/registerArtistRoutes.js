d56311236cf828f7d76f350fc90bf5fe01a3a1cd
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2021-10-27 19:38:44 +0300

    The artist account routes were added to the routes

--- routes/registerArtistRoutes.js
+++ routes/registerArtistRoutes.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-console */
 const express = require('express');
 const mongoose = require('mongoose');
+const multer = require('multer');
 const ArtistsReg = require('../models/artistRegModel');
 
 const router = express.Router();
@@ -11,12 +12,26 @@ router.get('/artistregistrationform', (req, res) => {
   res.render('artistregform');
 });
 
+// Instatiation of storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/imagefiles');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
 // Route to post the data from the artist registrationform page.
 
-router.post('/artistregistrationform', async (req, res) => {
+router.post('/artistregistrationform', upload.single('pictureupload'), async (req, res) => {
   console.log(req.body);
   try {
     const artistReg = new ArtistsReg(req.body);
+    artistReg.pictureupload = req.file.path;
+    console.log(artistReg);
+    console.log('This is the image you want to upload', req.file);
     await ArtistsReg.register(artistReg, req.body.password, (err) => {
       if (err) {
         throw err;
@@ -30,4 +45,19 @@ router.post('/artistregistrationform', async (req, res) => {
   }
 });
 
-module.exports = router; 
+// Route to go  to the  Ugaamux artist account.
+router.get('/artistsaccount', async (req, res) => {
+  if (req.session.user) {
+    try {
+      const users = await ArtistsReg.find();
+      res.render('artistaccount', { artists: users });
+    } catch {
+      res.status(400).send('Unable to find artist');
+    }
+  } else {
+    res.redirect('/login');
+  }
+
+});
+
+module.exports = router;
