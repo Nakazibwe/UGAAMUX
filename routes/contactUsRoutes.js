5e4ad1db456ddada5875a57013f51f195214b3b0
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2021-10-28 13:41:32 +0300

    Routes to the contactUs pages

---
+++ routes/contactUsRoutes.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+const express = require('express');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const Contactus = require('../models/contactUsModel');
+
+const router = express.Router();
+
+// Route to  get the contactus page (contactus.html)
+router.get('/contactUs', (req, res) => {
+  res.render('contactUs');
+});
+
+// Route to post data from the contactus form on the page.
+router.post('/contactUs', (req, res) => {
+  console.log(req.body);
+});
+
+module.exports = router;
