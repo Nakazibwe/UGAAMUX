77b1f60359855069ececf136eedb981a8ce88083
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2021-10-28 21:30:40 +0300

    A provision to upload pictures was created in async function

--- routes/registerComediansRoutes.js
+++ routes/registerComediansRoutes.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-unreachable */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 /* eslint-disable no-path-concat */
 /* eslint-disable prefer-template */
 const express = require('express');
 const mongoose = require('mongoose');
+const multer = require('multer');
 const ComedianReg = require('../models/comedianRegModel');
 
 const router = express.Router();
@@ -13,9 +15,51 @@ router.get('/comedianregistrationform', (req, res) => {
   res.render('comedianregform');
 });
 
+// Instatiation of storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/imagefiles');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
 // Route to post from  comedians registration page.
-router.post('/comedianregistrationform', (req, res) => {
+router.post('/comedianregistrationform', upload.single('uploadedpicture'), async (req, res) => {
   console.log(req.body);
+  try {
+    const comedianReg = new ComedianReg(req.body);
+    comedianReg.uploadedpicture = req.file.path;
+    console.log(comedianReg);
+    console.log('This is the image you want to upload', req.file);
+    await ComedianReg.register(comedianReg, req.body.password, (err) => {
+      if (err) {
+        throw err;
+        console.log('Data has not been posted', err);
+      }
+      res.redirect('/comedianinfo/comedianregistrationform');
+    });
+  } catch (err) {
+    res.status(400).send('Sorry! Data was not sent to DB');
+    console.log(err);
+  }
 });
 
-module.exports = router; 
+
+// Route to go  to the  Ugaamux private comedian account.
+router.get('/comedianaccount', async (req, res) => {
+  if (req.session.user) {
+    try {
+      const users = await ComedianReg.find();
+      res.render('comedianaccount', { comedians: users });
+    } catch {
+      res.status(400).send('Unable to find comedian');
+    }
+  } else {
+    res.redirect('/login');
+  }
+});
+
+module.exports = router;
