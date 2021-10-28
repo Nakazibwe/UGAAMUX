e2adf0ea2019f87006f345ba69a5affeeabbe8ba
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2021-10-28 21:29:56 +0300

    More routes were added and DB path for sending data was established

--- routes/registerClerksRoutes.js
+++ routes/registerClerksRoutes.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unreachable */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 const express = require('express');
@@ -10,8 +11,24 @@ const router = express.Router();
 router.get('/clerkregistrationform', (req, res) => {
   res.render('clerkregform');
 });
-router.post('/clerkregistrationform', (req, res) => {
+
+// Route to post data from the clerk form.
+router.post('/clerkregistrationform', async (req, res) => {
   console.log(req.body);
+  try {
+    const clerkReg = new ClerkReg(req.body);
+    console.log(clerkReg);
+    await ClerkReg.register(clerkReg, req.body.password, (err) => {
+      if (err) {
+        throw err;
+        console.log('Data has not been posted', err);
+      }
+      res.redirect('/clerkinfo/clerkregistrationform');
+    });
+  } catch (err) {
+    res.status(400).send('Sorry! Data was not sent to DB');
+    console.log(err);
+  }
 });
 
 module.exports = router;
