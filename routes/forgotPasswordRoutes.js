994a70e8c13dc16a8046bf237bcbc50e7721e43f
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2021-10-29 09:20:35 +0300

    The changes were made to the post route to sava data to DB

--- routes/forgotPasswordRoutes.js
+++ routes/forgotPasswordRoutes.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unreachable */
 /* eslint-disable no-multiple-empty-lines */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
@@ -16,10 +17,26 @@ router.get('/forgotpassword', (req, res) => {
 });
 
 // Route to post data from the forgot password page.
-router.post('/forgotpassword', (req, res) => {
+router.post('/forgotpassword', (req, res) => {  
   console.log(req.body);
 });
-
+router.post('/forgotpassword', async (req, res) => {
+  console.log(req.body);
+  try {
+    const forgotPassword = new ForgotPassword(req.body);
+    // console.log(forgotPassword);
+    await ForgotPassword.save(forgotPassword, (err) => {
+      if (err) {
+        throw err;
+        console.log('Data has not been posted', err);
+      }
+      res.redirect('/passwordreset/forgotpassword');
+    });
+  } catch (err) {
+    res.status(400).send('Sorry! Data was not sent to DB');
+    console.log(err);
+  }
+});
 
 
 
