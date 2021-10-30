a4cae60eaa1328ebe688138dc41ebbe6ab784366
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2021-10-30 18:59:00 +0300

    Logic was changed to enable admin receive inputs from the forms

--- routes/contactUsRoutes.js
+++ routes/contactUsRoutes.js
@@ -3,7 +3,8 @@
 /* eslint-disable no-console */
 const express = require('express');
 const mongoose = require('mongoose');
-
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 const Contactus = require('../models/contactUsModel');
 
 const router = express.Router();
@@ -12,24 +13,37 @@ const router = express.Router();
 router.get('/contactUs', (req, res) => {
   res.render('contactUs');
 });
+// Route to post contact information to be emailed to  Super admin .
+router.post('/contactUs', (req, res) => {
+  const transporter = nodemailer.createTransport({
+    service: 'hotmail',
+    auth: {
+      user: 'nakazibwe19grace@outlook.com',
+      pass: process.env.PASSWORD,
+    },
+  });
 
-// Route to post data from the contactus form on the page.
-router.post('/contactUs', async (req, res) => {
-  console.log(req.body);
-  try {
-    const contactus = new Contactus(req.body);
-    console.log(contactus);
-    await Contactus.save(contactus, (err) => {
-      if (err) {
-        throw err;
-        console.log('Data has not been posted', err);
-      }
-      res.redirect('/contactinfo/contactUs');
-    });
-  } catch (err) {
-    res.status(400).send('Sorry! Data was not sent to DB');
-    console.log(err);
-  }
+  const mailOptions = {
+    from: 'nakazibwe19grace@outlook.com',
+    to: 'nakazibwe24grace@outlook.com',
+    subject: ' EMAIL WITH  UGAAMUX CONTACTUS DETAILS ',
+    html: `<html><body><p>Firstname : ${req.body.firstname}</p><p>Lastname : ${req.body.lastname}</p><p> Email : ${req.body.email}</p><p>Message : ${req.body.message}</p></body></html>`,
+  };
+
+  transporter.sendMail(mailOptions, (err, data) => {
+    if (err) {
+      console.log(
+        'There was an error in sending the email with nodejs',
+      );
+      console.log(err);
+    } else {
+      console.log(data);
+      console.log(
+        'Email was sent successfully',
+      );
+    }
+  });
+  res.redirect('/contactinfo/contactUs');
 });
 
 module.exports = router;
