f9ce3e78ee83b85d4d04cfba37aedea9dd3d4949
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2021-11-01 10:25:13 +0300

    Admin dashboard

---
+++ public/javaScriptfiles/admindashboard.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+/* eslint-disable eol-last */
+$(document).ready(() => {
+  $('#sidebarCollapse').on('click', () => {
+    $('#sidebar').toggleClass('active');
+  });
+});
+
+/* eslint-disable eol-last */
+// $(document).ready(() => {
+//     $('#sidebarCollapse').on('click', () => {
+//       $('#sidebar').toggleClass('active');
+//     });
+//   });
+  
+  
