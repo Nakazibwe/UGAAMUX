9fc3ffe467b9f30951adda5ace73825c1f719816
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2021-11-03 00:18:12 +0300

    Changes were made to the post router and the personal account router

--- routes/registerArtistRoutes.js
+++ routes/registerArtistRoutes.js
@@ -1,10 +1,19 @@
+/* eslint-disable arrow-parens */
+/* eslint-disable eol-last */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-shadow */
+/* eslint-disable no-undef */
+/* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable object-shorthand */
 /* eslint-disable no-unreachable */
 /* eslint-disable no-console */
 const express = require('express');
 const mongoose = require('mongoose');
 const multer = require('multer');
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
 const ArtistsReg = require('../models/artistRegModel');
+const User = require('../models/UserModel');
 
 const router = express.Router();
 
@@ -25,21 +34,21 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 // Route to post the data from the artist registrationform page.
-
 router.post('/artistregistrationform', upload.single('pictureupload'), async (req, res) => {
   console.log(req.body);
   try {
     const artistReg = new ArtistsReg(req.body);
+    const user = new User(req.body);
     artistReg.pictureupload = req.file.path;
-    console.log(artistReg);
-    console.log('This is the image you want to upload', req.file);
-    await ArtistsReg.register(artistReg, req.body.password, (err) => {
+    // console.log(artistReg);
+    // console.log('This is the image you want to upload', req.file);
+    await artistReg.save();
+    await User.register(user, req.body.password, (err) => {
       if (err) {
         throw err;
-        console.log('Data has not been posted', err);
       }
-      // res.redirect('/artistinfo/artistregistrationform');
-      res.redirect('/registrationpage');
+
+      res.redirect('/clerkinfo/creativesregistration'); 
     });
   } catch (err) {
     res.status(400).send('Sorry! Data was not sent to DB');
@@ -48,28 +57,13 @@ router.post('/artistregistrationform', upload.single('pictureupload'), async (re
 });
 
 // Route to go  to the  Ugaamux artist account.
-// router.get('/artistpersonalaccount', async (req, res) => {
-//   // if (req.session.user) {
-//     try {
-//       const users = await ArtistsReg.find();
-//       // console.log(users);
-//       res.render('artistaccount', { users });
-//       // console.log('These are my artists', artists)
-//     } catch {
-//       res.status(400).send('Unable to find artist');
-//     }
-
-//   // } else {
-//   //   res.redirect('/login');
-//   // }
-// });
 router.get('/artistpersonalaccount', async (req, res) => {
   if (req.session.user) {
     try {
-      // const user = await ArtistsReg.findOne({ email: req.body.email });
+      const user = await ArtistsReg.findOne({ email: req.user.email });
       // console.log(users);
-      res.render('artistaccount', { user: req.user });
-    // console.log('These are my artists', artists)
+      res.render('artistaccount', { artist: user });
+      // console.log('These are my artists', artists)
     } catch {
       res.status(400).send('Unable to find artist');
     }
@@ -79,3 +73,5 @@ router.get('/artistpersonalaccount', async (req, res) => {
 });
 
 module.exports = router;
+
+// /clerkinfo/creativesregistration
