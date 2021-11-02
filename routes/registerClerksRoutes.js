6429e7b5ba044d1808063c620faea94c969c6f68
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2021-11-03 00:02:30 +0300

    Changes were made to the post router to the clerk registration

--- routes/registerClerksRoutes.js
+++ routes/registerClerksRoutes.js
@@ -4,33 +4,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const multer = require('multer');
+const User = require('../models/UserModel');
 const ClerkReg = require('../models/clerkRegModel');
 
 const router = express.Router();
+// Route to the page for clerk to register all other  creatives .
+router.get('/creativesregistration', (req, res) => {
+  res.render('registrationpage');
+});
+
 // Route to go the clerk registration page.
 router.get('/clerkregistrationform', (req, res) => {
   res.render('clerkregform');
 });
 
-// Route to the page for clerk to register all creatives .
-router.get('/creativesregistration', (req, res) => {
-  res.render('registrationpage');
-});
-
-
-
 // Route to post data from the clerk form.
 router.post('/clerkregistrationform', async (req, res) => {
   console.log(req.body);
   try {
+    const user = new User(req.body);
     const clerkReg = new ClerkReg(req.body);
     console.log(clerkReg);
-    await ClerkReg.register(clerkReg, req.body.password, (err) => {
+    await clerkReg.save();
+    await User.register(user, req.body.password, (err) => {
       if (err) {
         throw err;
-        console.log('Data has not been posted', err);
       }
-      // res.redirect('/clerkinfo/clerkregistrationform');
+
       res.redirect('/login');
     });
   } catch (err) {
