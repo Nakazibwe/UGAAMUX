2d0eb02911f8438ec891c8f1ebec13d2ff34c42a
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2021-11-03 00:25:24 +0300

    Trials for authentication

--- permissions/basicAuthentication.js
+++ permissions/basicAuthentication.js
@@ -1,9 +1,42 @@
-// function authUser(req, res, next) {
-//   if (req.user == null) {
-//     res.status(403);
-//     return res.send('Please login in ');
-//   }
-//   next();
-// }
+// /* eslint-disable func-names */
+// /* eslint-disable no-unused-vars */
+// const LocalStrategy = require('passport-local').Strategy;
+// const mongoose = require('mongoose');
+// const bcrypt = require('bcryptjs');
 
-// module.exports = authUser;
+// // Load ArtistReg Model.
+// const ArtistsReg = require('../models/artistRegModel');
+
+// module.exports = function (passport) {
+//     passport.use(
+//         new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+//             // Match User.
+//             ArtistsReg.findOne({ email })
+//                 .then((artist) => {
+//                     if (!artist) {
+//                         return done(null, false, { message: 'Email is not registered' });
+//                     }
+//                     // Match password.
+//                     bcrypt.compare(password, artist.password, (err, isMatch) => {
+//                         if (err) throw err;
+
+//                         if (isMatch) {
+//                             return done(null, artist);
+//                         }
+//                         return done(null, false, { message: 'Password Incorrect' });
+//                     });
+
+//                 });
+//         // .catch ((err) => console.log(err));
+// })
+//   );
+// passport.serializeUser((artist, done) => {
+//     done(null, artist.id);
+// });
+
+// passport.deserializeUser((id, done) => {
+//     ArtistsReg.findById(id, (err, artist) => {
+//         done(err, artist);
+//     });
+// });
+// };
