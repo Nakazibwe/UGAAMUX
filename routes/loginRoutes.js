3b9f9c96269bb708c93ef4ca572c90078385541c
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2021-11-03 18:42:43 +0300

    M ore routes were added to the login route

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -15,6 +15,7 @@ const ComedianReg = require('../models/comedianRegModel');
 const BandReg = require('../models/bandRegModel');
 const ClerkReg = require('../models/clerkRegModel');
 const User = require('../models/UserModel');
+const roles = require('./roles');
 
 const router = express.Router();
 
@@ -22,7 +23,7 @@ const router = express.Router();
 router.get('/login', (req, res) => {
   res.render('login');
 });
-// Route to login .
+// Route to login  post request.
 router.post('/login', passport.authenticate('local', { failureRedirect: '/login' }), async (req, res) => {
   req.session.user = req.user;
   // Login logic for the different users of the system .
@@ -38,10 +39,57 @@ router.post('/login', passport.authenticate('local', { failureRedirect: '/login'
       } else if (data.role == 'clerk') {
         res.redirect('/clerkinfo/creativesregistration');
       } else {
-        res.redirect('/');
+        res.send(' You are not an authorized  UGAAMUX user ');
       }
     })
     .catch((err) => { console.log(err); });
+  // const userrole = roles[req.user.role];
+  // if (userrole == 'artist') {
+  //   res.redirect('/artistinfo/artistpersonalaccount');
+  // }
+});
+
+router.get('/logout', (req, res) => {
+  req.session.destroy(() => {
+    res.redirect('/');
+  });
+});
+
+router.post('/deleteartist', async (req, res) => {
+  if (req.session.user) {
+    try {
+      await ArtistsReg.deleteOne({ _id: req.body.id });
+      await User.deleteOne({ _id: req.body.id });
+      res.redirect('back');
+
+    } catch (err){
+    res.send('You are unable to delete an artist ',err)
+    }
+
+  } else {
+    console.log('cant find session'); 
+    res.redirect('/login');
+  }
+});
+
+router.post('/deletecomedian', async (req, res) => {
+  if (req.session.user) {
+    try {
+       await 
+    } catch (err) {
+      res.send('You are unable to delete a comedian ', err)
+    }
+  }
+});
+
+router.post('/deleteartist', async (req, res) => {
+  if (req.session.user) {
+    try {
+     await 
+    } catch (err){
+    res.send('You are unable to delete an artist ',err)
+    }
+  }
 });
 
 module.exports = router;
