0e6d061da40cd04044d342c5cd05794020b82405
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2021-11-03 09:36:04 +0300

    More logic was added to the login router

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -9,6 +9,7 @@
 const express = require('express');
 const passport = require('passport');
 const bcrypt = require('bcryptjs');
+const connect = require('connect');
 const ArtistsReg = require('../models/artistRegModel');
 const ComedianReg = require('../models/comedianRegModel');
 const BandReg = require('../models/bandRegModel');
@@ -25,16 +26,13 @@ router.get('/login', (req, res) => {
 router.post('/login', passport.authenticate('local', { failureRedirect: '/login' }), async (req, res) => {
   req.session.user = req.user;
 
-  if ((req.user.email && req.user.password.match(User))&&(req.user.email && req.user.password.match(ArtistsReg))) {
+  if ((req.user.email && req.user.password.match(User)) && (req.user.email && req.user.password.match(ArtistsReg))) {
     res.redirect('/artistinfo/artistpersonalaccount');
-  }
-  if ((req.user.email && req.user.password.match(User)) && (req.user.email && req.user.password.match(ComedianReg))) {
+  } if ((req.user.email && req.user.password.match(User)) && (req.user.email && req.user.password.match(ComedianReg))) {
     res.redirect('/comedianinfo/comedianaccount');
-  }
-  if ((req.user.email && req.user.password.match(User)) && (req.user.email && req.user.password.match(BandReg))) {
+  } if ((req.user.email && req.user.password.match(User)) && (req.user.email && req.user.password.match(BandReg))) {
     res.redirect('/bandinfo/bandaccount');
-  }
-  if ((req.user.email && req.user.password.match(User)) && (req.user.email && req.user.password.match(ClerkReg))) {
+  } if ((req.user.email && req.user.password.match(User)) && (req.user.email && req.user.password.match(ClerkReg))) {
     res.redirect('/clerkinfo/creativesregistration');
   }
 });
