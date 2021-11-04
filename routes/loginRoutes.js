c990ce7cca6d766f1da8af8c665e7d03f67627b5
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2021-11-04 21:39:42 +0300

    Delete routes were added to the file

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -61,13 +61,11 @@ router.post('/deleteartist', async (req, res) => {
       await ArtistsReg.deleteOne({ _id: req.body.id });
       await User.deleteOne({ _id: req.body.id });
       res.redirect('back');
-
-    } catch (err){
-    res.send('You are unable to delete an artist ',err)
+    } catch (err) {
+      res.send('You are unable to delete an artist ', err);
     }
-
   } else {
-    console.log('cant find session'); 
+    console.log('cant find session');
     res.redirect('/login');
   }
 });
@@ -75,21 +73,68 @@ router.post('/deleteartist', async (req, res) => {
 router.post('/deletecomedian', async (req, res) => {
   if (req.session.user) {
     try {
-       await 
+      await ComedianReg.deleteOne({ _id: req.body.id });
+      await User.deleteOne({ _id: req.body.id });
+      res.redirect('back');
     } catch (err) {
-      res.send('You are unable to delete a comedian ', err)
+      res.send('You are unable to delete a comedian ', err);
     }
+  } else {
+    console.log('cant find session');
+    res.redirect('/login');
   }
 });
 
-router.post('/deleteartist', async (req, res) => {
+router.post('/deleteband', async (req, res) => {
   if (req.session.user) {
     try {
-     await 
-    } catch (err){
-    res.send('You are unable to delete an artist ',err)
+      await BandReg.deleteOne({ _id: req.body.id });
+      await User.deleteOne({ _id: req.body.id });
+      res.redirect('back');
+    } catch (err) {
+      res.send('You are unable to delete a band ', err);
     }
+  } else {
+    console.log('cant find session');
+    res.redirect('/login');
   }
+});
+
+// router.get('/admindashboard', async (req, res) => {
+//   try {
+//     const artists = await ArtistsReg.find();
+//     const bands = await BandReg.find();
+//     const comedians = await ComedianReg.find();
+//     const clerks = await ClerkReg.find();
+//     // console.log(artists);
+//     // console.log(bands);
+//     // console.log(comedians);
+//     // console.log(clerks);
+//     res.render('admindashboard',
+//       {
+//         artists: artists,
+//         comedians: comedians,
+//         clerks: clerks,
+//         bands: bands,
+//       });
+//   } catch {
+//     res.status(400).send('Unable to find artist');
+//   }
+// });
+
+router.get('/updateartist', async (req, res) => {
+  // if (req.session.user) {
+  try {
+    // const artists = await ArtistsReg.updateOne({ _id: req.body.id });
+    // const users = await User.updateOne({ _id: req.body.id });
+    res.render('updateartist');
+  } catch {
+    res.status(400).send('Unable to update  artist');
+  }
+  // } else {
+  //   console.log('cant find session');
+  //   res.redirect('/login');
+  // }
 });
 
 module.exports = router;
