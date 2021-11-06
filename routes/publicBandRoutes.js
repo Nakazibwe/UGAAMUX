2906e8ce7665f4873ea4fe7382ae0fc2cfe0f4d5
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2021-11-06 21:31:45 +0300

    Logic was added to ensure that data can be queryed and the search bar can be used to search.

--- routes/publicBandRoutes.js
+++ routes/publicBandRoutes.js
@@ -1,15 +1,31 @@
+/* eslint-disable key-spacing */
+/* eslint-disable object-shorthand */
+/* eslint-disable no-undef */
+/* eslint-disable no-empty */
 /* eslint-disable no-multiple-empty-lines */
 /* eslint-disable eol-last */
 /* eslint-disable no-console */
 /* eslint-disable no-unused-expressions */
 /* eslint-disable no-unused-vars */
 const express = require('express');
+const BandReg = require('../models/bandRegModel');
 
 const router = express.Router();
+
 // Route to get the bands page.
-router.get('/bands', (req, res) => {
-  res.render('bands');
-});
+router.get('/bands', async (req, res) => {
+  try {
+    let users = await BandReg.find();
+    if (req.query.BandName) {
+      users = await BandReg.find({ BandName: req.query.BandName });
+    } else if (req.query.twitter) {
+      users = await BandReg.find({ twitter: req.query.twitter });
+    }
+    res.render('bands', { bands:users });
+  } catch (err) {
+    res.status(400).send('Band  is Unavailable', err);
+  }
+}); 
 
 
 
