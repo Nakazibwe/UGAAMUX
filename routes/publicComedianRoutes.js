a763f04276722dfea30814afb793522588d47e30
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2021-11-06 21:29:09 +0300

    Logic was added to query data from the database to ensure the search input works well

--- routes/publicComedianRoutes.js
+++ routes/publicComedianRoutes.js
@@ -4,13 +4,23 @@
 /* eslint-disable no-unused-expressions */
 /* eslint-disable no-unused-vars */
 const express = require('express');
+const ComedianReg = require('../models/comedianRegModel');
 
 const router = express.Router();
 // Route to get to the comedians Page.
-router.get('/comedians', (req, res) => {
-  res.render('comedians');
+router.get('/comedians', async (req, res) => {
+  try {
+    let users = await ComedianReg.find();
+    if (req.query.stagename) {
+      users = await ComedianReg.find({ stagename: req.query.stagename });
+    } else if (req.query.twitter) {
+      users = await ComedianReg.find({ twitter: req.query.twitter });
+    }
+    res.render('comedians', { comedians: users });
+  } catch (err) {
+    res.status(400).send('Comedian  is Unavailable', err);
+  }  
 });
-
 
 
 module.exports = router;
