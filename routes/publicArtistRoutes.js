b25e071b90fd439eaae2f3bf3a62721f2d922774
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2021-11-07 23:38:35 +0300

    Changes were made to the search bar

--- routes/publicArtistRoutes.js
+++ routes/publicArtistRoutes.js
@@ -1,21 +1,26 @@
+/* eslint-disable prefer-template */
+/* eslint-disable prefer-destructuring */
 /* eslint-disable no-multiple-empty-lines */
 /* eslint-disable eol-last */
 /* eslint-disable no-console */
 /* eslint-disable no-unused-expressions */
 /* eslint-disable no-unused-vars */
 const express = require('express');
+const Sequelize = require('sequelize');
 
+const Op = Sequelize.Op;
 const router = express.Router();
 const ArtistsReg = require('../models/artistRegModel');
 
+
+
 // Route to get to the Artists Page.
-router.get('/artists', async (req, res) => { 
+router.get('/artists', async (req, res) => {
   try {
     let users = await ArtistsReg.find();
-    if (req.query.stageName) {
-      users = await ArtistsReg.find({ stageName: req.query.stageName });
-    } else if (req.query.twitter) {
-      users = await ArtistsReg.find({ twitter: req.query.twitter });
+    if (req.query.search) {
+      console.log(req.query.search[0]);
+      users = await ArtistsReg.find({ stageName: req.query.search });
     }
     res.render('artists', { artists: users });
   } catch (err) {
@@ -23,6 +28,12 @@ router.get('/artists', async (req, res) => {
   }
 });
 
+// router.get('/artists', (req, res) => {
+//   const { stagename } = req.query;
+//   ArtistsReg.find({ where: { stageName: { [Op.like]: term } } })
+//     .then((artists) => res.render('artists', { artists }))
+//     .catch((err) => console.log(err));
+// });
 
 
 module.exports = router;
