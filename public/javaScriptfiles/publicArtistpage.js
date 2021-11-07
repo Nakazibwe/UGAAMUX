a68dcdbd15f8aa6de2eb2549ed5a034192846580
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2021-11-07 23:32:23 +0300

    File for public javascript on public artist pages

---
+++ public/javaScriptfiles/publicArtistpage.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+const searchBar = document.getElementById('search');
+console.log(searchBar);
+searchBar.addEventListener('keyup', (e) => {
+  console.log(e);
+});
