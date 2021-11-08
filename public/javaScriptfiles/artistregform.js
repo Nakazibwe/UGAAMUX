6b3d031976d10931493e654a6328be1caf0d6c44
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2021-11-08 19:22:06 +0300

    Prevent default was added to the validations

--- public/javaScriptfiles/artistregform.js
+++ public/javaScriptfiles/artistregform.js
@@ -61,7 +61,8 @@ const phoneFormat = /^\d{12}$/;
 const twitterFormat = /(^|[^@\w])@(\w{1,15})\b/;
 const emailFormat = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
 
-const validate = () => {
+const validate = (event) => {
+  let isValid = true;
   // Validating the first name .
   const artistfirstname = Firstname.value.trim();
   if (artistfirstname == '') {
@@ -69,21 +70,25 @@ const validate = () => {
     FirstNameERROR.innerHTML = 'First Name Field Required';
     FirstNameERROR.style = 'color:red ';
     Firstname.focus();
+    isValid = false;
   } else if (!(artistfirstname.length > 1)) {
     Firstname.style.border = '1px solid red';
     FirstNameERROR.innerHTML = 'First name is supposed to be more than one letter';
     FirstNameERROR.style = 'color:red ';
     Firstname.focus();
+    isValid = false;
   } else if (!artistfirstname.match(nonumber)) {
     FirstNameERROR.innerHTML = 'First Name should not  contain numbers';
     FirstNameERROR.style = 'color:red ';
     Firstname.style.border = '1px solid red';
     Firstname.focus();
+    isValid = false;
   } else if (!artistfirstname.match(capitalize)) {
     FirstNameERROR.innerHTML = 'First Name should start with a capital letter';
     FirstNameERROR.style = 'color:red ';
     Firstname.style.border = '1px solid red';
     Firstname.focus();
+    isValid = false;
   } else {
     FirstNameERROR.innerHTML = ' Field correctly filled ';
     FirstNameERROR.style = 'color:green ';
@@ -96,21 +101,25 @@ const validate = () => {
     LastNameERROR.innerHTML = 'Last Name Field Required ';
     LastNameERROR.style = 'color:red ';
     Lastname.focus();
+    isValid = false;
   } else if (!(artistlastname.length > 1)) {
     Lastname.style.border = '1px solid red';
     LastNameERROR.innerHTML = 'Last name is supposed to be more than one letter';
     LastNameERROR.style = 'color:red ';
     Lastname.focus();
+    isValid = false;
   } else if (!artistlastname.match(nonumber)) {
     LastNameERROR.innerHTML = 'Last Name should not  contain numbers';
     LastNameERROR.style = 'color:red ';
     Lastname.style.border = '1px solid red';
     Lastname.focus();
+    isValid = false;
   } else if (!artistlastname.match(capitalize)) {
     LastNameERROR.innerHTML = 'Last  Name should start with a capital letter ';
     LastNameERROR.style = 'color:red ';
     Lastname.style.border = '1px solid red';
     Lastname.focus();
+    isValid = false;
   } else {
     LastNameERROR.innerHTML = 'Field correctly filled ';
     LastNameERROR.style = 'color:green ';
@@ -123,21 +132,25 @@ const validate = () => {
     StageNameERROR.innerHTML = ' Stagename Field Required ';
     StageNameERROR.style = 'color:red';
     Stagename.focus();
+    isValid = false;
   } else if (!(artiststagename.length > 1)) {
     Stagename.style.border = '1px solid red';
     StageNameERROR.innerHTML = 'Stage  name is supposed to be more than one letter';
     StageNameERROR.style = 'color:red ';
     Stagename.focus();
+    isValid = false;
   } else if (!artiststagename.match(capitalize)) {
     StageNameERROR.innerHTML = 'Stage Name should start with a capital letter';
     StageNameERROR.style = 'color:red ';
     Stagename.style.border = '1px solid red';
     Stagename.focus();
+    isValid = false;
   } else if (!artiststagename.match(alphaNumeric)) {
     Stagename.style.border = '1px solid red';
     StageNameERROR.innerHTML = ' Stagename Format should be Alphanumeric';
     StageNameERROR.style = 'color:red';
     Stagename.focus();
+    isValid = false;
   } else {
     StageNameERROR.innerHTML = 'Field correctly filled ';
     StageNameERROR.style = 'color:green ';
@@ -150,11 +163,13 @@ const validate = () => {
     ArtistIDERROR.innerHTML = ' Artist ID Field Required';
     ArtistIDERROR.style = 'color:red';
     ArtistID.focus();
+    isValid = false;
   } else if (!artistID.match(systemIDFormat)) {
     ArtistID.style.border = '1px solid red';
     ArtistIDERROR.innerHTML = ' Artist ID Format entered  is wrong';
     ArtistIDERROR.style = 'color:red';
     ArtistID.focus();
+    isValid = false;
   } else {
     ArtistIDERROR.innerHTML = 'Field correctly filled ';
     ArtistIDERROR.style = 'color:green ';
@@ -167,11 +182,13 @@ const validate = () => {
     PhonenumberERROR.style = 'color:red';
     Telephone.style.border = '1px solid red';
     Telephone.focus();
+    isValid = false;
   } else if (!artistTelephone.match(phoneFormat)) {
     PhonenumberERROR.innerHTML = 'Phone number Format Entered is Wrong';
     PhonenumberERROR.style = 'color:red';
     Telephone.style.border = '1px solid red';
     Telephone.focus();
+    isValid = false;
   } else {
     PhonenumberERROR.innerHTML = 'Field correctly filled ';
     PhonenumberERROR.style = 'color:green ';
@@ -184,11 +201,13 @@ const validate = () => {
     TwitterERROR.style = 'color:red';
     Twitterhandle.style.border = '1px solid red';
     Twitterhandle.focus();
+    isValid = false;
   } else if (!artistTwitter.match(twitterFormat)) {
     TwitterERROR.innerHTML = 'Twitter Handle  doesnot match Twitter format ';
     TwitterERROR.style = 'color:red';
     Twitterhandle.style.border = '1px solid red';
     Twitterhandle.focus();
+    isValid = false;
   } else {
     TwitterERROR.innerHTML = 'Field correctly filled ';
     TwitterERROR.style = 'color:green ';
@@ -201,11 +220,13 @@ const validate = () => {
     EmailERROR.style = 'color:red';
     ArtistEmail.style.border = '1px solid red';
     ArtistEmail.focus();
+    isValid = false;
   } else if (!artistEmail.match(emailFormat)) {
     EmailERROR.innerHTML = 'Email Format entered is Wrong';
     EmailERROR.style = 'color:red';
     ArtistEmail.style.border = '1px solid red';
     ArtistEmail.focus();
+    isValid = false;
   } else {
     EmailERROR.innerHTML = 'Field correctly filled ';
     EmailERROR.style = 'color:green ';
@@ -217,6 +238,7 @@ const validate = () => {
     DateofBirthERROR.style = 'color:red';
     DateofBirth.style.border = '1px solid red';
     DateofBirth.focus();
+    isValid = false;
   } else {
     DateofBirthERROR.innerHTML = 'Field correctly filled ';
     DateofBirthERROR.style = 'color:green ';
@@ -230,21 +252,25 @@ const validate = () => {
     BasedDistrictERROR.style = 'color:red';
     BasedDistrict.style.border = '1px solid red';
     BasedDistrict.focus();
+    isValid = false;
   } else if (!(district.length > 1)) {
     BasedDistrict.style.border = '1px solid red';
     BasedDistrictERROR.innerHTML = 'Based District is supposed to be more than one letter';
     BasedDistrictERROR.style = 'color:red ';
     BasedDistrict.focus();
+    isValid = false;
   } else if (!district.match(nonumber)) {
     BasedDistrictERROR.innerHTML = 'Based District  should not  contain numbers';
     BasedDistrictERROR.style = 'color:red ';
     BasedDistrict.style.border = '1px solid red';
     BasedDistrict.focus();
+    isValid = false;
   } else if (!district.match(capitalize)) {
     BasedDistrictERROR.innerHTML = ' Based District should start with a capital letter';
     BasedDistrictERROR.style = 'color:red ';
     BasedDistrict.style.border = '1px solid red';
     BasedDistrict.focus();
+    isValid = false;
   } else {
     BasedDistrictERROR.innerHTML = ' Field correctly filled ';
     BasedDistrictERROR.style = 'color:green ';
@@ -257,11 +283,13 @@ const validate = () => {
     NationalNINERROR.style = 'color:red';
     NationalIDNIN.style.border = '1px solid red';
     NationalIDNIN.focus();
+    isValid = false;
   } else if (!nationalNIN.match(nationalIDFormat)) {
     NationalNINERROR.innerHTML = 'National ID NIN  Format Wrong';
     NationalNINERROR.style = 'color:red';
     NationalIDNIN.style.border = '1px solid red';
     NationalIDNIN.focus();
+    isValid = false;
   } else {
     NationalNINERROR.innerHTML = 'Field correctly filled ';
     NationalNINERROR.style = 'color:green ';
@@ -273,6 +301,7 @@ const validate = () => {
     DateCareerStartERROR.style = 'color:red';
     DateCareerStart.style.border = '1px solid red';
     DateCareerStart.focus();
+    isValid = false;
   } else {
     DateCareerStartERROR.innerHTML = 'Field correctly filled ';
     DateCareerStartERROR.style = 'color:green ';
@@ -285,21 +314,25 @@ const validate = () => {
     ArtistLocationERROR.style = 'color:red';
     ArtistLocation.style.border = '1px solid red';
     ArtistLocation.focus();
+    isValid = false;
   } else if (!(artistLocation.length > 1)) {
     ArtistLocation.style.border = '1px solid red';
     ArtistLocationERROR.innerHTML = 'Artist Location  is supposed to be more than one letter';
     ArtistLocationERROR.style = 'color:red ';
     ArtistLocation.focus();
+    isValid = false;
   } else if (!artistLocation.match(nonumber)) {
     ArtistLocationERROR.innerHTML = 'Artist Location   should not  contain numbers';
     ArtistLocationERROR.style = 'color:red ';
     ArtistLocation.style.border = '1px solid red';
     ArtistLocation.focus();
+    isValid = false;
   } else if (!artistLocation.match(capitalize)) {
     ArtistLocationERROR.innerHTML = ' Artist Location  should start with a capital letter';
     ArtistLocationERROR.style = 'color:red ';
     ArtistLocation.style.border = '1px solid red';
     ArtistLocation.focus();
+    isValid = false;
   } else {
     ArtistLocationERROR.innerHTML = ' Field correctly filled ';
     ArtistLocationERROR.style = 'color:green ';
@@ -311,11 +344,13 @@ const validate = () => {
     NumberofAlbumsERROR.style = 'color:red';
     NumberofAlbums.style.border = '1px solid red';
     NumberofAlbums.focus();
+    isValid = false;
   } else if (NumberofAlbums.value < 0) {
     NumberofAlbumsERROR.innerHTML = ' Invalid Number Input ';
     NumberofAlbumsERROR.style = 'color:red';
     NumberofAlbums.style.border = '1px solid red';
     NumberofAlbums.focus();
+    isValid = false;
   } else {
     NumberofAlbumsERROR.innerHTML = ' Field correctly filled ';
     NumberofAlbumsERROR.style = 'color:green ';
@@ -327,11 +362,13 @@ const validate = () => {
     NumberofSongsERROR.style = 'color:red';
     NumberofSongs.style.border = '1px solid red';
     NumberofSongs.focus();
+    isValid = false;
   } else if (NumberofSongs.value < 0) {
     NumberofSongsERROR.innerHTML = ' Invalid Number Input ';
     NumberofSongsERROR.style = 'color:red';
     NumberofSongs.style.border = '1px solid red';
     NumberofSongs.focus();
+    isValid = false;
   } else {
     NumberofSongsERROR.innerHTML = ' Field correctly filled ';
     NumberofSongsERROR.style = 'color:green ';
@@ -344,21 +381,25 @@ const validate = () => {
     GenreERROR.style = 'color:red';
     ArtistGenre.style.border = '1px solid red';
     ArtistGenre.focus();
+    isValid = false;
   } else if (!(artistGenre.length > 1)) {
     ArtistGenre.style.border = '1px solid red';
     GenreERROR.innerHTML = 'Artist Genre  is supposed to be more than one letter';
     GenreERROR.style = 'color:red ';
     ArtistGenre.focus();
+    isValid = false;
   } else if (!artistGenre.match(capitalize)) {
     GenreERROR.innerHTML = ' Artist Genre  should start with a capital letter';
     GenreERROR.style = 'color:red ';
     ArtistGenre.style.border = '1px solid red';
     ArtistGenre.focus();
+    isValid = false;
   } else if (!artistGenre.match(alphaNumeric)) {
     ArtistGenre.style.border = '1px solid red';
     GenreERROR.innerHTML = ' Artist Genre Format should be Alphanumeric';
     GenreERROR.style = 'color:red';
     ArtistGenre.focus();
+    isValid = false;
   } else {
     GenreERROR.innerHTML = 'Field correctly filled';
     GenreERROR.style = 'color:green ';
@@ -368,6 +409,7 @@ const validate = () => {
   if (MaleGender.checked == false && FemaleGender.checked == false) {
     GenderERROR.innerHTML = 'Gender Field Required ';
     GenderERROR.style = 'color:red';
+    isValid = false;
   } else {
     GenderERROR.innerHTML = 'Field correctly filled';
     GenderERROR.style = 'color:green ';
@@ -377,6 +419,7 @@ const validate = () => {
     PictureFileERROR.innerHTML = 'Artist Photo Field Required';
     PictureFileERROR.style = 'color:red';
     PictureFile.focus();
+    isValid = false;
   } else {
     PictureFileERROR.innerHTML = 'Field correctly filled';
     PictureFileERROR.style = 'color:green ';
@@ -388,11 +431,13 @@ const validate = () => {
     PasswordERROR.innerHTML = 'Password Field Required';
     PasswordERROR.style = 'color:red';
     Password.focus();
+    isValid = false;
   } else if (!(userpassword.length > 8)) {
     PasswordERROR.innerHTML = 'Enter password with more than 8 characters';
     PasswordERROR.style = 'color:red';
     Password.style.border = '1px solid red';
     Password.focus();
+    isValid = false;
   } else {
     Password.style.border = '1px solid green';
     PasswordERROR.innerHTML = 'Password Field Filled';
@@ -405,6 +450,7 @@ const validate = () => {
     UserroleERROR.style = 'color:red';
     Role.style.border = '1px solid red';
     Role.focus();
+    isValid = false;
   } else {
     UserroleERROR.innerHTML = 'Field Correctly Filled';
     UserroleERROR.style = 'color:green';
@@ -417,12 +463,21 @@ const validate = () => {
     ArtistbioERROR.style = 'color:red';
     Artistbio.style.border = '1px solid red';
     Artistbio.focus();
+    isValid = false;
   } else {
     ArtistbioERROR.innerHTML = 'Field Correctly Filled';
     ArtistbioERROR.style = 'color:green';
     Artistbio.style.border = '1px solid green';
   }
+  if (!isValid) {
+    event.preventDefault();
+    return false;
+  }
 };
 
 const Artistform = document.getElementById('form-id');
-Artistform.addEventListener('submit', validate);
+// Artistform.addEventListener('submit', validate);
+
+Artistform.addEventListener('submit', (event) => {
+  validate(event);
+});
